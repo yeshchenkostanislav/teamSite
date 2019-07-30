84dced372fe1c1ab87fb8f5ecc8cdad024fcf492
Stanislav Yeshchenko <yeshchenkostanislav@gmail.com>
2019-07-31 00:34:34 +0200

    интерактив добавлен

--- app/elements/common-elements/header/header.js
+++ app/elements/common-elements/header/header.js
@@ -1,5 +1,5 @@
  var mySwiper = new Swiper('.swiper-container', {
-   // Optional parameters
+
    direction: 'vertical',
    loop: true,
    speed: 700,
@@ -10,8 +10,49 @@
    sensitivity: 1,
    invert: true,
 
-   // If we need pagination
    pagination: {
      el: '.swiper-pagination',
+     type: 'bullets',
    }
  })
+
+ let menuItem = $('.header__nav-link');
+
+ $(menuItem).click(function () {
+
+   $(menuItem).removeClass('header__nav-link_active');
+   $(this).addClass('header__nav-link_active');
+
+   let num = $(this).attr('data-num');
+   mySwiper.slideTo(num, 1000, false);
+
+   exchangeText();
+
+ })
+
+ let actSlide;
+
+ mySwiper.on('slideChange', function () {
+
+   actSlide = mySwiper.activeIndex;
+
+   $(menuItem).removeClass('header__nav-link_active');
+   $(`.header__nav-link[data-num=` + actSlide + `]`).addClass('header__nav-link_active');
+
+   exchangeText();
+
+   if (actSlide == 0) {
+     $('.nav .header__nav-link:last').addClass('header__nav-link_active');
+   } else if (actSlide == 7) {
+     $('.nav .header__nav-link:first').addClass('header__nav-link_active');
+   }
+
+ });
+
+ function exchangeText() {
+   if ($('.header__nav-link_active').attr('data-num') == 6 || actSlide == 0) {
+     $('.footer__text').text('scroll up');
+   } else {
+     $('.footer__text').text('scroll down');
+   }
+ }
--- build/js/custom.js
+++ build/js/custom.js
@@ -1,21 +1,60 @@
 'use strict';
 
 $('document').ready(function () {
-  var mySwiper = new Swiper('.swiper-container', {
-    // Optional parameters
-    direction: 'vertical',
-    loop: true,
-    speed: 700,
-    simulateTouch: true,
-    mousewheel: {
-      invert: true
-    },
-    sensitivity: 1,
-    invert: true,
+   var mySwiper = new Swiper('.swiper-container', {
 
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination'
-    }
-  });
+      direction: 'vertical',
+      loop: true,
+      speed: 700,
+      simulateTouch: true,
+      mousewheel: {
+         invert: true
+      },
+      sensitivity: 1,
+      invert: true,
+
+      pagination: {
+         el: '.swiper-pagination',
+         type: 'bullets'
+      }
+   });
+
+   var menuItem = $('.header__nav-link');
+
+   $(menuItem).click(function () {
+
+      $(menuItem).removeClass('header__nav-link_active');
+      $(this).addClass('header__nav-link_active');
+
+      var num = $(this).attr('data-num');
+      mySwiper.slideTo(num, 1000, false);
+
+      exchangeText();
+   });
+
+   var actSlide = void 0;
+
+   mySwiper.on('slideChange', function () {
+
+      actSlide = mySwiper.activeIndex;
+
+      $(menuItem).removeClass('header__nav-link_active');
+      $('.header__nav-link[data-num=' + actSlide + ']').addClass('header__nav-link_active');
+
+      exchangeText();
+
+      if (actSlide == 0) {
+         $('.nav .header__nav-link:last').addClass('header__nav-link_active');
+      } else if (actSlide == 7) {
+         $('.nav .header__nav-link:first').addClass('header__nav-link_active');
+      }
+   });
+
+   function exchangeText() {
+      if ($('.header__nav-link_active').attr('data-num') == 6 || actSlide == 0) {
+         $('.footer__text').text('scroll up');
+      } else {
+         $('.footer__text').text('scroll down');
+      }
+   }
 });
